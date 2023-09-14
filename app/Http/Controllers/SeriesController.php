7cee6353b2a6fad4b82f12c5a39762b28bd7be47
Kevin Toledo <kevin.toledo@mysmartclinic.com.br>
2023-09-14 20:14:57 -0300

    Second Course 1st commit

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -2,17 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Serie;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SeriesController extends Controller
 {
     public function index(Request $request)
     {
-        $series = [
-            'Punisher',
-            'Gray\'s Anatomy',
-            'Lost'
-        ];
+        $series = Serie::query()->orderBy('nome')->get();
         return view('series.index')->with('series', $series);
     }
 
@@ -23,11 +21,8 @@ class SeriesController extends Controller
 
     public function store(Request $request)
     {
-        $nomeSerie = $request->input('nome');
-        if(\DB::insert('INSERT INTO series (nome) VALUES (?)', [$nomeSerie]))
-            return "OK";
-        else
-            return "Deu erro";
-        
+        Serie::create($request->all());
+
+        return redirect('/series');
     }
 }
--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -2,6 +2,6 @@
     <a href="/series/criar" class="btn btn-dark mb-2">Adicionar</a>
     <ul class="list-group">
         <?php foreach($series as $serie): ?>
-            <li class="list-group-item"><?= $serie; ?></li>
+            <li class="list-group-item"><?= $serie->nome; ?></li>
         <?php endforeach;?>  
     </x-layout>
